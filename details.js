a256a9dd3028a8c801e9aa2bb094100aaa81a19c
KavyaCOder1 <97831532+KavyaCOder1@users.noreply.github.com>
2023-07-30 19:46:06 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '6329058372:AAEojH3iTeOdOfFOFjGGnP3Vc846KKh-bLc',
+    token: '6574005156:AAHGtrEDV8Dk_SN686x2HoFm0o6sazfhg1Y',
     admins: [5556717138],
     mongo_url: 'mongodb+srv://manishprajapati7517:R4gO0q3MFPDbjqKp@kavyatg.zgsh5nz.mongodb.net/?retryWrites=true&w=majority',
 }
